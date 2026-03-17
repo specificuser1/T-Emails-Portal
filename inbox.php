84d4d455c6c0d9aa52a93b830d5cc9ea7d37b38d
specificuser1 <motorolag14rx@gmail.com>
2026-03-17 21:43:22 +0500

    Update inbox.php

--- inbox.php
+++ inbox.php
@@ -1,35 +1,35 @@
 <?php
-$email = $_GET['email'] ?? "";
-$filename = "mails/" . md5($email) . ".json";
-
-$mails = [];
-if (file_exists($filename)) {
-    $mails = json_decode(file_get_contents($filename), true);
-}
+session_start();
+if (!isset($_SESSION["temp_email"])) header("Location: index.php");
+$email = $_SESSION["temp_email"];
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Inbox - <?php echo $email; ?></title>
-    <link rel="stylesheet" href="style.css">
-    <meta http-equiv="refresh" content="10">
+    <title>Inbox – <?php echo $email; ?></title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/popup.css">
+    <script src="assets/script.js"></script>
 </head>
-<body>
-    <div class="box">
-        <h2>Inbox: <?php echo $email; ?></h2>
-        <a href="index.php" class="btn">Generate New Mail</a>
 
-        <?php if (empty($mails)) { ?>
-            <p>No mails yet. Waiting...</p>
-        <?php } else { ?>
-            <?php foreach ($mails as $mail) { ?>
-                <div class="mail">
-                    <h3><?php echo $mail['subject']; ?></h3>
-                    <small>From: <?php echo $mail['from']; ?></small>
-                    <p><?php echo nl2br($mail['message']); ?></p>
-                </div>
-            <?php } ?>
-        <?php } ?>
+<body class="glass">
+    <div class="box">
+        <h2>Your Inbox</h2>
+        <p>Email: <b><?php echo $email; ?></b></p>
+
+        <input type="text" id="search" placeholder="Search mail..." onkeyup="loadInbox()" />
+
+        <div id="inbox"></div>
+
+        <button class="btn" onclick="downloadInbox()">Download Emails</button>
+        <a class="btn" href="index.php">New Email</a>
     </div>
+
+<script>
+setInterval(loadInbox, 4000);
+window.onload = loadInbox;
+</script>
+
 </body>
 </html>
