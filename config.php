08ca421511b963db98ccdafae166932db8e1a181
specificuser1 <motorolag14rx@gmail.com>
2026-03-17 21:39:39 +0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+// Allowed custom domains
+$MAIL_DOMAINS = [
+    "smail.pk",
+    "tempraza.io",
+    "mailxpress.xyz",
+    "tmail.free",
+    "randommail.bio",
+    "wemail.abc",
+    "mymail.com",
+    "gmáil.com"
+];
+
+// Admin credentials
+$ADMIN_USER = "root";
+$ADMIN_PASS = "admin89";   // change before hosting!
+
+// Webhook security token
+$WEBHOOK_TOKEN = "subhan_secure_987xyz";
